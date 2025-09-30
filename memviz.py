60828f5f5c642668994ef7e6940860916a267d1c
Stefan Schmidt <snst@meek.de>
2025-09-30 15:37:20 +0200

    fixes

--- memviz.py
+++ memviz.py
@@ -10,22 +10,34 @@ bars = []
 
 
 colors = [
-    "#e6194b", "#3cb44b", "#c6af19", "#4363d8", "#f58231",
-    "#911eb4", "#46f0f0", "#f032e6", "#808080",
-    "#008080", "#9a6324", "#800000", "#808000",    
+    "#e6194b",
+    "#3cb44b",
+    "#c6af19",
+    "#4363d8",
+    "#f58231",
+    "#911eb4",
+    "#46f0f0",
+    "#f032e6",
+    "#808080",
+    "#008080",
+    "#9a6324",
+    "#800000",
+    "#808000",
 ]
 color_index = 0
+
 
 def get_color():
     global color_index
     color_index += 1
-    return colors[color_index%len(colors)]
+    return colors[color_index % len(colors)]
 
 
-class AddrList():
+class AddrList:
     def __init__(self):
         self.start = []
         self.end = []
+
 
 def add_addr(addr):
     if not addr in address_dict:
@@ -35,6 +47,7 @@ def add_addr(addr):
         items = address_dict[addr]
     return items
 
+
 def add_addr_start(entry):
     entries = add_addr(entry.start).start
     for i in range(0, len(entries)):
@@ -42,6 +55,7 @@ def add_addr_start(entry):
             entries.insert(i, entry)
             return
     entries.append(entry)
+
 
 def add_addr_end(entry):
     entries = add_addr(entry.end).end
@@ -59,9 +73,9 @@ class RamEntry:
         self.size = self.get(data, "size")
         self.name = data.get("name", "")
         if self.end is None and self.size is not None:
-            self.end = self.start +  self.size - 1
+            self.end = self.start + self.size - 1
         if self.size is None and self.end is not None:
-            self.size = self.end +  self.start
+            self.size = self.end + self.start
         add_addr_start(self)
         add_addr_end(self)
 
@@ -78,6 +92,7 @@ def load_ram_layout(yaml_path):
     for item in items:
         entries.append(RamEntry(item))
 
+
 def add_start(ram, y):
     global bars
     col = get_color()
@@ -87,6 +102,7 @@ def add_start(ram, y):
             return col
     bars.append((ram, y, col))
     return col
+
 
 def get_start(ram):
     global bars
@@ -98,6 +114,7 @@ def get_start(ram):
             return i, ret, ret_color
     return None
 
+
 def draw_layout(svg_path):
     font_height = 14
     x = 0
@@ -106,54 +123,72 @@ def draw_layout(svg_path):
     bar_x = 80
     addr_color = "#000000"
     addr_space = 20
-    
-    text_y_offset = font_height/2-2
+
+    text_y_offset = font_height / 2 - 2
     elements: list[svg.Element] = []
     elements2: list[svg.Element] = []
     elements_addr: list[svg.Element] = []
     last_addr = None
     for addr, items in sorted(address_dict.items()):
-        
-#        if last_addr is not None and addr - 1 != last_addr:
         if addr - 1 != last_addr:
             y += addr_space
-            #gap = addr - last_addr
-            #text = svg.Text(x=12, y=y-addr_space/2-2, text=f"GAP [0x{gap:x}, {gap}]", font_size=font_height, fill="#3E3E3E")
-            #elements2.append(text)
 
-        addr_y = y
+        start_y = y
+        addr_y = start_y + text_y_offset
 
-        text = svg.Text(x=x, y=addr_y+text_y_offset, text=f"0x{addr:08x}", font_size=font_height, fill=addr_color)
-        elements_addr.append(text)
         for item in items.end:
-            i, start_y, col = get_start(item)
-            text = svg.Text(x=0, y=y+text_y_offset, text=f"- {item.name}", font_size=font_height, fill=col)
+            i, start_bar_y, col = get_start(item)
+            text = svg.Text(
+                x=0,
+                y=y + text_y_offset,
+                text=f"- {item.name}",
+                font_size=font_height,
+                fill=col,
+            )
             elements2.append(text)
-            rect = svg.Rect(x=x+bar_x + i*bar_width, y=start_y, width=bar_width, height=addr_y-start_y, fill=col)#, stroke="#333")
+            addr_y = text.y
+            # bar
+            bar_ext = 7
+            rect = svg.Rect(
+                x=x + bar_x + i * bar_width,
+                y=start_bar_y - (font_height / 2),
+                width=bar_width,
+                height=start_y - start_bar_y + font_height * (len(items.end) + 0),
+                fill=col,
+            )  # , stroke="#333")
             elements.append(rect)
             y += font_height
         for item in items.start:
-            col = add_start(item, addr_y)
-            text = svg.Text(x=0, y=y+text_y_offset, text=f"+ {item.name} [0x{(item.size):x}, {item.size}]", font_size=14, fill=col)
+            col = add_start(item, start_y)
+            text = svg.Text(
+                x=0,
+                y=y + text_y_offset,
+                text=f"+ {item.name} [0x{(item.size):x}, {item.size}]",
+                font_size=14,
+                fill=col,
+            )
             elements2.append(text)
             y += font_height
+
+        text = svg.Text(
+            x=x, y=addr_y, text=f"0x{addr:08x}", font_size=font_height, fill=addr_color
+        )
+        elements_addr.append(text)
+
         last_addr = addr
-
-
 
     desc_x = max(bar_x + len(bars) * bar_width + 5, bar_x)
     for e in elements2:
         e.x += desc_x
 
-    bg = svg.Rect(x=0, y=0, width=300, height=y-addr_space/2, fill="#ffffff")
+    bg = svg.Rect(x=0, y=0, width=300, height=y - addr_space / 2 + 20, fill="#ffffff")
     canvas = svg.SVG(
         viewBox=svg.ViewBoxSpec(0, 0, bg.width, bg.height),
-        elements=[bg]+elements+elements2+elements_addr,
+        elements=[bg] + elements + elements2 + elements_addr,
     )
-    
+
     with open(svg_path, "w") as f:
         f.write(str(canvas))
-
 
 
 if __name__ == "__main__":
